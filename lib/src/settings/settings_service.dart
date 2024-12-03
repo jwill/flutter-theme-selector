d121f8dcde0beccc2f4b1aa6d17c47ee4e40ecea
James Williams <66931+jwill@users.noreply.github.com>
2024-12-03 14:40:07 -0800

    Reduced font scale limit to 2.0

--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -55,26 +55,6 @@ class SettingsSignalsService {
     }
 
 
-  Future <ColorSeed> colorSeed2() async {
-    Color? seed = (await prefs.getInt(COLOR_SEED))?.toColor();
-    Color? secondarySeed = (await prefs.getInt(COLOR_SECONDARY_SEED)).toColor();
-    Color? tertiarySeed = (await prefs.getInt(COLOR_TERTIARY_SEED)).toColor();
-    Color? neutralSeed = (await prefs.getInt(COLOR_NEUTRAL_SEED)).toColor();
-    Color? neutralVariantSeed = (await prefs.getInt(COLOR_NV_SEED)).toColor();
-    Color? errorSeed = (await prefs.getInt(COLOR_ERROR_SEED)).toColor();
-
-    if (seed == null) {
-      seed = Color(0x6750A4FF);
-    }
-
-    return ColorSeed("", seed);
-  }
-
-
-
-
-
-
     Future <bool> monochrome() async {
       bool? isMonochrome = await prefs.getBool(MONOCHROME);
       if (isMonochrome == null) {
@@ -83,26 +63,6 @@ class SettingsSignalsService {
         return isMonochrome;
     }
 
-    // Future<void> updateDisplayFont(String newValue) async {
-    //   String? displayFont = await prefs.getString(DISPLAY_FONT);
-    //   if (newValue != displayFont) {
-    //     prefs.setString(DISPLAY_FONT, newValue);
-    //   }
-    // }
-    //
-    // Future<void> updateBodyFont(String newValue) async {
-    //   String? bodyFont = await prefs.getString(BODY_FONT);
-    //   if (newValue != bodyFont) {
-    //     prefs.setString(BODY_FONT, newValue);
-    //   }
-    // }
-
-    Future<void> updateVariant(String newValue) async {
-      String? variant = await prefs.getString(VARIANT);
-      if (newValue != variant) {
-        prefs.setString(VARIANT, newValue);
-      }
-    }
   }
 
 extension Converters on String {
@@ -118,5 +78,3 @@ extension Converters on String {
     return ThemeMode.system;
   }
 }
-
-
